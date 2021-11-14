15e2ec55a5d6ea5a828f80480928a01239631c8f
GeonHeeAhn <justamoment@ewhain.net>
2021-11-15 02:00:01 +0900

    test commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Fooding</title>
+
+	<!-- <link rel="stylesheet" href="/css/main.css"> -->
+</head>
+<body>
+	<!-- <?php
+		include $_SERVER["DOCUMENT_ROOT"]."/php/nav.php";
+	?> -->
+
+	<section id="main">
+		<?php
+			include_once $_SERVER["DOCUMENT_ROOT"]."/php/mysqli.inc";
+            $restaurantList = $mysqli->query("select * from resturant");
+            if($restaurantList) {
+                while($row = $restaurantList->fetch_array(MYSQLI_ASSOC){
+                    print"<p>$row[name]</p>";
+                }
+            }
+		?>
+	</section>
+
+</body>
+</html>
