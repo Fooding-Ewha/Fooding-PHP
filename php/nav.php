42dd61fe239bc75eb1c3d071300e4e085a448f8a
SojeongLee <kather0220@ewhain.net>
2021-11-17 03:11:57 +0900

    style: edit restaurant list display for rank page

--- php/nav.php
+++ php/nav.php
@@ -1,3 +1,4 @@
 <a href='/login'>Login</a>
 <a href='/rank'>Ranking</a>
 <a href='/signup'>SignUp</a>
+<a href='/restaurant'>Restaurant</a>
--- rank/index.php
+++ rank/index.php
@@ -6,15 +6,15 @@
 	<meta charset="utf-8">
 	<title>Fooding</title>
 
-	<!-- <link rel="stylesheet" href="/css/main.css"> -->
+	<link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
    
-  
+<section id="main" style="display: flex; flex-direction: column;">
  <?php
  include_once $_SERVER['DOCUMENT_ROOT'] . '/php/mysqli.inc';
  $query_string = $_GET['value'];
- print "<h3>Ranking for $query_string</h3>";
+ print "<h3>Ranking for $query_string</h3>"; // 위에 나오는 Ranking for ~ 문구
  if (isset($_GET['region'])) {
    $region = $_GET['region'];
    $restaurant_list = $mysqli->query(
@@ -27,13 +27,16 @@
    );
  }
  ?>
-  <div class="container" style="display: grid; grid-template-columns: 300px 300px; padding: 1px;">
+ <!--여기서부터 그리드 아이템-->
+  <div class="container" style="display: grid; grid-template-columns: 300px 300px; padding: 10px;">
   <?php if ($restaurant_list->num_rows > 0) {
     while ($row = $restaurant_list->fetch_array(MYSQLI_ASSOC)) {
-      print "<div class='item'>$row[name] <br> $row[address]</div>";
+      $onclick_query = "location.href='/restaurant?name=$row[restaurant_id];";
+      print "<a href='/restaurant?id=$row[restaurant_id]' style='text-decoration: none; color: inherit;'>$row[name] <br> $row[address] </a>"; // 아이템 담는 a 태그
     }
   } else {
     echo 'No Restaurant exists for the clicked value';
   } ?></div>
+  </section>
 </body>
 </html>
