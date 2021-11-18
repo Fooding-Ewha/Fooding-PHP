947b958eeafecf6fc86a8396c4a0ac81f56dc433
SojeongLee <kather0220@ewhain.net>
2021-11-19 01:13:32 +0900

    feat: implement ranking function for restaurants

--- rank/index.php
+++ rank/index.php
@@ -18,7 +18,10 @@
  if (isset($_GET['region'])) {
    $region = $_GET['region'];
    $restaurant_list = $mysqli->query(
-     "SELECT * FROM Restaurant WHERE region_id='" . $region . "'"
+     // advanced sql # 2 RANK
+     "SELECT *, rank() OVER(order by score desc) AS ranking FROM Restaurant WHERE region_id='" .
+       $region .
+       "'"
    );
  } elseif (isset($_GET['category'])) {
    $category = $_GET['category'];
@@ -32,7 +35,8 @@
   <?php if ($restaurant_list->num_rows > 0) {
     while ($row = $restaurant_list->fetch_array(MYSQLI_ASSOC)) {
       $onclick_query = "location.href='/restaurant?name=$row[restaurant_id];";
-      print "<a href='/restaurant?id=$row[restaurant_id]' style='text-decoration: none; color: inherit;'>$row[name] <br> $row[address] </a>"; // 아이템 담는 a 태그
+      // 그리드 아이템 요소들이 아래 a 태그에 다 들어가서 <br> 로만 줄을 나누어놨어 근데 css 하려면 변수 빼서 태그 나눠도 좋겠어
+      print "<a href='/restaurant?id=$row[restaurant_id]' style='text-decoration: none; color: inherit;'>$row[ranking] <br> $row[name] <br> $row[address] </a>"; // 아이템 담는 a 태그
     }
   } else {
     echo 'No Restaurant exists for the clicked value';
