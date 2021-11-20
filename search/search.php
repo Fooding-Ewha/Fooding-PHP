68792607612cccd9cba477b1cafc1e5d649bca34
SojeongLee <kather0220@ewhain.net>
2021-11-20 14:29:27 +0900

    style: add restaurant image on search page

--- search/search.php
+++ search/search.php
@@ -4,7 +4,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Fooding</title>
+	<title>Search Result</title>
 
 	<link rel="stylesheet" href="../css/main.css">
 </head>
@@ -15,7 +15,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/php/mysqli.inc';
 if (isset($_GET['query'])) {
   $query = $_GET['query'];
   ($result = $mysqli->query(
-    "SELECT r.name as `name`, r.address as `address` FROM Restaurant r join Category c on r.category_id=c.category_id WHERE r.name LIKE '%" .
+    "SELECT r.name as `name`, r.address as `address`, r.image as `image` FROM Restaurant r join Category c on r.category_id=c.category_id WHERE r.name LIKE '%" .
       $query .
       "%' OR c.name LIKE '%" .
       $query .
@@ -24,7 +24,7 @@ if (isset($_GET['query'])) {
 } elseif (isset($_GET['keyword'])) {
   $keyword = $_GET['keyword'];
   ($result = $mysqli->query(
-    "SELECT r.name as `name`, r.address as `address` FROM Restaurant r join Keyword k on r.keyword_id=k.keyword_id WHERE k.name LIKE '%" .
+    "SELECT r.name as `name`, r.address as `address`, r.image as `image` FROM Restaurant r join Keyword k on r.keyword_id=k.keyword_id WHERE k.name LIKE '%" .
       $keyword .
       "%'"
   )) or die($mysqli->error);
@@ -36,9 +36,10 @@ if ($result->num_rows > 0) {
     echo 'Search result for ' . $keyword;
   }
 
-  while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-    echo '<p><h3>' . $row['name'] . '</h3>' . $row['address'] . '</p>';
-  }
+  while ($row = $result->fetch_array(MYSQLI_ASSOC)) { ?>
+    <img src=<?php echo "$row[image]"; ?> style='width: 140px; height: 140px; border-radius: 50%; margin-right: 50px;'></img>
+    <h3><?php echo "$row[name]"; ?><br><?php echo "$row[address]"; ?></h3>;
+    <?php }
 } else {
   echo 'No results';
 }
