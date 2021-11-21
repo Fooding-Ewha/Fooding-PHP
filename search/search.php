708d4881b015b9399bb67db71cb16d1cfba6e978
SojeongLee <kather0220@ewhain.net>
2021-11-21 16:34:39 +0900

    feat: add link to search result

--- search/search.php
+++ search/search.php
@@ -15,7 +15,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/php/mysqli.inc';
 if (isset($_GET['query'])) {
   $query = $_GET['query'];
   ($result = $mysqli->query(
-    "SELECT r.name as `name`, r.address as `address`, r.image as `image`, c.name AS `category` FROM Restaurant r join Category c on r.category_id=c.category_id join Region g ON r.region_id = g.region_id WHERE r.name LIKE '%" .
+    "SELECT r.restaurant_id as `restaurant_id`, r.name as `name`, r.address as `address`, r.image as `image`, c.name AS `category` FROM Restaurant r join Category c on r.category_id=c.category_id join Region g ON r.region_id = g.region_id WHERE r.name LIKE '%" .
       $query .
       "%' OR c.name LIKE '%" .
       $query .
@@ -26,7 +26,7 @@ if (isset($_GET['query'])) {
 } elseif (isset($_GET['keyword'])) {
   $keyword = $_GET['keyword'];
   ($result = $mysqli->query(
-    "SELECT r.name as `name`, r.address as `address`, r.image as `image` FROM Restaurant r join Keyword k on r.keyword_id=k.keyword_id WHERE k.name LIKE '%" .
+    "SELECT r.restaurant_id as `restaurant_id`, r.name as `name`, r.address as `address`, r.image as `image` FROM Restaurant r join Keyword k on r.keyword_id=k.keyword_id WHERE k.name LIKE '%" .
       $keyword .
       "%'"
   )) or die($mysqli->error);
@@ -40,7 +40,11 @@ if ($result->num_rows > 0) {
 
   while ($row = $result->fetch_array(MYSQLI_ASSOC)) { ?>
     <img src=<?php echo "$row[image]"; ?> style='width: 140px; height: 140px; border-radius: 50%; margin-right: 50px;'></img> <!--레스토랑 이미지랑 텍스트 하나씩 보여줌-->
-    <h3><?php echo "$row[name]"; ?><br><?php echo "$row[address]"; ?></h3>;
+    <a href="/restaurant?id=<?php echo '$row[
+      restaurant_id
+    ]'; ?>"><h3><?php echo "$row[name]"; ?><br><?php echo "$row[address]"; ?></h3>;
+    </a>
+    
     <?php }
 } else {
   echo 'No results';
