d36fabb731aea5d1b21404990b44c240fdf74d5c
GeonHeeAhn <justamoment@ewhain.net>
2021-11-21 18:07:19 +0900

    style: changeinfo page

--- changeinfo/index.php
+++ changeinfo/index.php
@@ -12,17 +12,35 @@ $user_info = $result->fetch_array();
 	<meta charset="utf-8">
 	<title>Information Change</title>
 
-	<!-- <link rel="stylesheet" href="/css/main.css"> -->
+	<link rel="stylesheet" href="../css/main.css">
 </head>
 <body>
+<body>
+		<section class="main-container">
+		<?php include $_SERVER['DOCUMENT_ROOT'] . '/php/nav.php'; ?>
+			<header>
+				<a href='/'>
+					<img class="logo" src="../public/logo.png"/>
+				</a>
+				<form class="search-box" action="/search/search.php" method="GET">
+					<input class="search-button" type="submit" value="" />
+					<input class="search-input" type="text" name="query" />
+				</form>
+			</header>
 	<section id="main">
-		<form method="POST" action="changeInfo.php">
-            id: <input type="text" name="user_id" placeholder="id" value=<?php echo "$user_info[id]"; ?> disabled=true></input>
-            password: <input type="password" name="original_password" placeholder="original password"></input>
-            new password: <input type ="password" name = "new_password" placeholder="new password ( Type original one if there's no change )" style = 'width: 400px;'></input>
-            nickname: <input type="text" name="user_name" placeholder="nickname" value=<?php echo "$user_info[user_name]"; ?>></input>
-            <input type="submit" name="submit"></input>
+		<form style='margin-bottom:50px;'method="POST" action="changeInfo.php">
+            <div class='info'>
+            id : <input class='login-input'type="text"style='margin-right:10px; margin-left: 10px;'name="user_id" placeholder="id" value=<?php echo "$user_info[id]"; ?> disabled=true></input>
+            password : <input class='login-input'type="password" style='margin-right:10px; margin-left: 10px;'name="original_password" placeholder="original password"></input>
+</div>
+            new password: <input class='login-input'type ="password" style='margin-right:10px; margin-left: 10px; width: 340px; margin-bottom: 15px;'name = "new_password" placeholder="new password ( Type original one if there's no change )" style = 'width: 400px;'></input>
+            nickname: <input class='login-input'type="text" style='margin-right:10px; margin-left: 10px;'name="user_name" placeholder="nickname" value=<?php echo "$user_info[user_name]"; ?>></input>
+            <input style='margin-top:25px;'class="login-button" type="submit" name="submit" value='submit'></input>
         </form>
 	</section>
+    <footer>
+				<button class="top-button"/>
+			</footer>
+            </section>
 </body>
 </html>
