a4e81edb0361b2b67e3442eeea71671b235f2818
GeonHeeAhn <justamoment@ewhain.net>
2021-11-21 17:57:44 +0900

    style:nav bar

--- php/nav.php
+++ php/nav.php
@@ -8,30 +8,30 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/php/mysqli.inc'; ?>
 <div class="container" style="display: flex; flex-direction: column; padding: 15px;">
 
 <?php if (isset($_SESSION['id'])) {
-  print "<a href='/logout'>Logout</a>";
-  print '<a href=/withdraw>Withdraw</a>';
-  print '<a href=/changeinfo>Change User Information</a>';
+  print "<a href='/logout' style='text-decoration:none;'>Logout</a>";
+  print "<a href='/withdraw'style='text-decoration:none;'>Withdraw</a>";
+  print "<a href='/changeinfo' style='text-decoration:none;'>Change User Information</a>";
 } else {
-  print "<a href='/login'>Login</a>";
+  print "<a href='/login'style='text-decoration:none;'>Login</a>";
 } ?>
 <h3>Region</h3>
 	<?php
  $region_list = $mysqli->query('select * from Region');
  if ($region_list) {
    while ($row = $region_list->fetch_array(MYSQLI_ASSOC)) {
-     print "<a href='/rank?region=$row[region_id]&value=$row[name]'>$row[name]</a>";
+     print "<a href='/rank?region=$row[region_id]&value=$row[name]'style='text-decoration:none;'>$row[name]</a>";
    }
  }
  ?>
    </div>
    
-   <div class="container" style="display: flex; flex-direction: column; ">
+   <div class="container" style="display: flex; flex-direction: column; margin-bottom:10px;margin-left:20px;">
    <h3>Category</h3>
 	<?php
  $category_list = $mysqli->query('select * from Category');
  if ($category_list) {
    while ($row = $category_list->fetch_array(MYSQLI_ASSOC)) {
-     print "<a href='/rank?category=$row[category_id]&value=$row[name]'>$row[name]</a>";
+     print "<a href='/rank?category=$row[category_id]&value=$row[name]' style='text-decoration:none;'>$row[name]</a>";
    }
  }
  ?>
