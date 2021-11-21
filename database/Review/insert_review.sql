8c59ed47b7613a5539d27f7e3d24b5b4672a0093
SojeongLee <kather0220@ewhain.net>
2021-11-21 19:46:13 +0900

    chore: add more sql files and data insert

---
+++ database/Review/insert_review.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Review` (`review_id`, `restaurant_id`, `user_id`, `score`, `comment`, `create_time`, `update_time`) VALUES (39, 25, 15, 0, 'satisfied', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `Review` (`review_id`, `restaurant_id`, `user_id`, `score`, `comment`, `create_time`, `update_time`) VALUES (40, 6, 15, 5, 'nice place to go', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `Review` (`review_id`, `restaurant_id`, `user_id`, `score`, `comment`, `create_time`, `update_time`) VALUES (41, 2, 15, 3, 'not bad', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `Review` (`review_id`, `restaurant_id`, `user_id`, `score`, `comment`, `create_time`, `update_time`) VALUES (42, 9, 15, 5, 'good taste', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `Review` (`review_id`, `restaurant_id`, `user_id`, `score`, `comment`, `create_time`, `update_time`) VALUES (43, 9, 1, 0, 'goooooood', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
---
+++ database/User/insert_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `User` (`user_id`, `user_name`, `id`, `password`, `create_time`, `update_time`) VALUES (16, 'lilii', 'test2', '12355', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `User` (`user_id`, `user_name`, `id`, `password`, `create_time`, `update_time`) VALUES (15, '거닝', 'testt', '12345', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `User` (`user_id`, `user_name`, `id`, `password`, `create_time`, `update_time`) VALUES (1, 'admin', 'admin', 'admin', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
