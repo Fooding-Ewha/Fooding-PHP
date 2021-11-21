648f4b89f4c0839e5b6bcd2b1c20b3581fa9eb17
GeonHeeAhn <justamoment@ewhain.net>
2021-11-21 15:48:43 +0900

    style: search result page

--- search/search.php
+++ search/search.php
@@ -9,7 +9,17 @@
 	<link rel="stylesheet" href="../css/main.css">
 </head>
 <body>
-
+<section class="main-container">
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/php/nav.php'; ?>
+	<header>
+		<a href='/'>
+			<img class="logo" src="../public/logo.png"/>
+		</a>
+		<form class="search-box" action="/search/search.php" method="GET">
+			<input class="search-button" type="submit" value="" />
+			<input class="search-input" type="text" name="query" />
+		</form>
+	<div style="display: flex; flex-direction: column; width: 100%; height: 440px;align-items: center;">
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/php/mysqli.inc';
 if (isset($_GET['query'])) {
@@ -33,18 +43,50 @@ if (isset($_GET['query'])) {
 }
 if ($result->num_rows > 0) {
   if (isset($query)) {
-    echo 'Search result for ' . $query;
+    echo "<div style='display: flex; '><h3 style='margin:5px; margin-top: 15px;'>Search result for </h3><h3 style='color: #efb43e; margin-top: 15px;margin-left:0;'> $query</h3></div>";
   } else {
-    echo 'Search result for ' . $keyword;
+    echo "<div style='display: flex;'><h3 style='margin:5px; margin-top: 15px;'>Search result for </h3><h3 style='color: #efb43e; margin-top: 15px;margin-left:0;'> $keyword</h3></div>";
   }
-
+}
+?>
+<div class="search-result-container" >
+<?php
+if (isset($_GET['query'])) {
+  $query = $_GET['query'];
+  ($result = $mysqli->query(
+    "SELECT r.name as `name`, r.address as `address`, r.image as `image`, c.name AS `category` FROM Restaurant r join Category c on r.category_id=c.category_id join Region g ON r.region_id = g.region_id WHERE r.name LIKE '%" .
+      $query .
+      "%' OR c.name LIKE '%" .
+      $query .
+      "%' OR g.name LIKE '%" .
+      $query .
+      "%';"
+  )) or die($mysqli->error);
+} elseif (isset($_GET['keyword'])) {
+  $keyword = $_GET['keyword'];
+  ($result = $mysqli->query(
+    "SELECT r.name as `name`, r.address as `address`, r.image as `image` FROM Restaurant r join Keyword k on r.keyword_id=k.keyword_id WHERE k.name LIKE '%" .
+      $keyword .
+      "%'"
+  )) or die($mysqli->error);
+}
+if ($result->num_rows > 0) {
   while ($row = $result->fetch_array(MYSQLI_ASSOC)) { ?>
+  <div style='width: 40%;
+  box-sizing: border-box; height: 160px; margin-top: 15px;margin-bottom: 15px; padding: 15px;border-radius: 20px; margin-left: 10px; margin-right: 10px;background-color: #fcfaf6; box-shadow: 0px 1px 30px rgba(145, 145, 145, 0.2); display: flex;'>
     <img src=<?php echo "$row[image]"; ?> style='width: 140px; height: 140px; border-radius: 50%; margin-right: 50px;'></img> <!--레스토랑 이미지랑 텍스트 하나씩 보여줌-->
-    <h3><?php echo "$row[name]"; ?><br><?php echo "$row[address]"; ?></h3>;
+    <h3><?php echo "$row[name]"; ?><br><?php echo "$row[address]"; ?></h3></div>
     <?php }
 } else {
   echo 'No results';
 }
-?>
+?></div>
+</div>
+</div>
+ <footer>
+		<button class="top-button"/>
+	</footer>
+  </div>
+</section>
 </body>
 </html>
